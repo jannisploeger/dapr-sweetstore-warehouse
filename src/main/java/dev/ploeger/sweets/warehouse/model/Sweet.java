c02175d9a07e4881c4f350a981efefc461c06e75
Jannis Plöger <jannis.ploeger@gmail.com>
2025-02-20 15:26:26 +0100

    added .gitignore and other files

---
+++ src/main/java/dev/ploeger/sweets/warehouse/model/Sweet.java
@@ -0,0 +1,5 @@
+package dev.ploeger.sweets.warehouse.model;
+
+public record Sweet(String name, double price, int quantity) {
+
+}
---
+++ src/test/java/dev/ploeger/sweets/warehouse/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package dev.ploeger.sweets.warehouse;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
